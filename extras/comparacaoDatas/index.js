251b65ec4a7021c8627f03b5176fc96b10427cce
athena272 <guilhermera272@gmail.com>
2023-03-31 10:32:22 -0300

    Create index.js

---
+++ extras/comparacaoDatas/index.js
@@ -0,0 +1,33 @@
+function testarComparacao() {
+    //Obter valores de data e converte para um objeto Date
+    const experienceDataInicio = new Date(document.getElementById("experience-dataInicio").value)
+    const experienceDataFim = new Date(document.getElementById("experience-datafim").value)
+
+    const graduationDataInicio = new Date(document.getElementById("graduation-dataInicio").value);
+    const graduationDataFim = new Date(document.getElementById("graduation-datafim").value);
+
+    // Verificar se são válidos
+    const isExperienceValid = experienceDataInicio <= experienceDataFim
+    const isGraduationValid = graduationDataInicio <= graduationDataFim;
+
+    // Exibir o resultado da comparação experiencia
+    const experience = document.querySelector("#experience");
+    experience.innerText = `Experiência: ${isExperienceValid ? "Válido" : "Inválido"}`
+    changeColor({ isValid: isExperienceValid, element: experience })
+
+    // Exibir o resultado da comparação graduation
+    const graduation = document.querySelector("#graduation");
+    graduation.innerText = `Graduação: ${isGraduationValid ? "Válido" : "Inválido"}`
+    changeColor({ isValid: isGraduationValid, element: graduation })
+
+}
+
+function changeColor({ isValid, element }) {
+    if (isValid) {
+        element.classList.add('valid')
+        element.classList.remove('invalid')
+    } else {
+        element.classList.add('invalid')
+        element.classList.remove('valid')
+    }
+}
