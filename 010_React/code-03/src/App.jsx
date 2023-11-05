0e23e9e98cd5ea10050220e6042e7a9d7890bf9d
athena272 <guilhermera272@gmail.com>
2023-11-05 12:30:42 -0300

    add project

--- 010_React/code-03/src/App.jsx
+++ 010_React/code-03/src/App.jsx
@@ -1,8 +1,17 @@
 import Card from './components/Card/Card'
+import posterStarWars from '/assets/star-wars.jpg'
+import empireStrike from '/assets/esb-poster.jpg'
+import returnJedi from '/assets/rotj-poster.jpg'
+
+
 import './App.css'
 
 const App = () => (
-    <Card />
+    <>
+        <Card title="Pôster: Star Wars (1977)" imgPoster={posterStarWars} />
+        <Card title="Pôster: Empire Strikes Back (1980)" imgPoster={empireStrike} />
+        <Card title="Pôster: Return of the Jedi (1983)" imgPoster={returnJedi} />
+    </>
 )
- 
+
 export default App
--- 010_React/code-03/src/components/Card/Card.jsx
+++ 010_React/code-03/src/components/Card/Card.jsx
@@ -1,17 +1,22 @@
-import posterImg from '/assets/star-wars.jpg'
 import './Card.css'
+import PropTypes from 'prop-types';
 
-const Card = () => (
+const Card = ({ title, imgPoster }) => (
     <div className="container">
-        <img className="poster" src={posterImg} alt="Star Wars poster" />
+        <img className="poster" src={imgPoster} alt="Star Wars poster" />
         <div>
-            <h2 className="title">Pôster do filme Star Wars</h2>
+            <h2 className="title">{title}</h2>
 
             <p className="description">Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho A3. Uma ótima recordação de um dos mais icônicos filmes de todos os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que você decidir pendurar. Não perca a chance de adicionar essa linda memória ao seu acervo!</p>
             <button className="button">Comprar agora</button>
         </div>
     </div>
 )
+
+Card.propTypes = {
+    title: PropTypes.string,
+    imgPoster: PropTypes.string
+}
 
 export default Card
 
