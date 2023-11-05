220df5abd862c5c0596ea944f2c16ab5ca160b80
athena272 <guilhermera272@gmail.com>
2023-11-05 13:09:01 -0300

    Update App.jsx

--- 010_React/code-04/src/App.jsx
+++ 010_React/code-04/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Profile from './components/Profile/Profile'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <div className="app">
+    <Profile
+      avatar="https://randomuser.me/api/portraits/men/75.jpg"
+      name="John Doe"
+      bio="Full-stack javascript developer at Acme Inc."
+      email="john.doe@email.com"
+      phone="+5511987654321"
+      githubUrl="https://github.com"
+      linkedinUrl="https://linkedin.com"
+      twitterUrl="https://twitter.com"
+    />
+  </div>
+)
 
 export default App
