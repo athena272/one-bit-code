d3022d5281038c6fba61aee8cef5d9d84236b8c5
athena272 <guilhermera272@gmail.com>
2023-11-12 19:34:00 -0300

    Update Profile.jsx

--- 010_React/code-07/src/components/Profile/Profile.jsx
+++ 010_React/code-07/src/components/Profile/Profile.jsx
@@ -4,29 +4,36 @@ import Title from '../Title/Title';
 import ProfileSection from '../ProfileSection/ProfileSection';
 import LinkButton from '../LinkButton/LinkButton';
 
-const Profile = ({ avatar, name, bio, phone, email, githubUrl, linkedinUrl, twitterUrl }) => (
-    <div className="container">
-        <img className="avatar" src={avatar} alt={name} />
-        <Title>
-            <span>Name: {name}</span>
+const Profile = ({ avatar, name, bio, phone, email, githubUrl, linkedinUrl, twitterUrl }) => {
+    function handleClick(ev) {
+        console.log(ev)
+        alert("Você agora está seguindo!")
+    }
 
-            <button>Follow</button>
-        </Title>
-        <ProfileSection>{bio}</ProfileSection>
-        <ProfileSection>{phone}</ProfileSection>
-        <ProfileSection>{email}</ProfileSection>
-        <ProfileSection
-            id="links-section"
-            data-test="some value"
-            aria-label="social links"
-            className="links"
-        >
-            <LinkButton url={githubUrl}>GitHub</LinkButton>
-            <LinkButton url={linkedinUrl}>LinkedIn</LinkButton>
-            <LinkButton url={twitterUrl}>Twitter</LinkButton>
-        </ProfileSection>
-    </div>
-)
+    return (
+        <div className="container">
+            <img className="avatar" src={avatar} alt={name} />
+            <Title>
+                <span>Name: {name}</span>
+
+                <button className='followButton' onClick={handleClick}>Follow</button>
+            </Title>
+            <ProfileSection>{bio}</ProfileSection>
+            <ProfileSection>{phone}</ProfileSection>
+            <ProfileSection>{email}</ProfileSection>
+            <ProfileSection
+                id="links-section"
+                data-test="some value"
+                aria-label="social links"
+                className="links"
+            >
+                <LinkButton url={githubUrl}>GitHub</LinkButton>
+                <LinkButton url={linkedinUrl}>LinkedIn</LinkButton>
+                <LinkButton url={twitterUrl}>Twitter</LinkButton>
+            </ProfileSection>
+        </div>
+    )
+}
 
 Profile.propTypes = {
     avatar: PropTypes.string,
