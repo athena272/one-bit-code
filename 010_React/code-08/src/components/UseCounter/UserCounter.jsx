4fd3eae2327ea73ff2efe998c9a28c26d22b60d9
athena272 <guilhermera272@gmail.com>
2023-11-13 02:28:48 -0300

    Update UserCounter.jsx

--- 010_React/code-08/src/components/UseCounter/UserCounter.jsx
+++ 010_React/code-08/src/components/UseCounter/UserCounter.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
-export default function useValue() {
+export function useAdd() {
     const [count, setCount] = useState(0)
 
     const increment = () => setCount(count + 1)
 
     return [count, increment]
 }
+
+export function useSub() {
+    const [count, setCount] = useState(0)
+
+    const decrement = () => setCount(count - 1)
+
+    return [count, decrement]
+}
+
