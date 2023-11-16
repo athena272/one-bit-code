b3925168215d0e5d02673780af3131a39a84ef6a
athena272 <guilhermera272@gmail.com>
2023-11-16 04:50:45 -0300

    Create Input.jsx

---
+++ 010_React/code-09/src/components/Input/Input.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import PropTypes from "prop-types"
+
+const Input = ({ passwordSize, setPasswordSize }) => (
+    <input
+        type="number"
+        id="passwordSize"
+        min={1}
+        max={50}
+        value={passwordSize}
+        onChange={(ev) => setPasswordSize(ev.target.value)}
+    />
+
+)
+
+Input.propTypes = {
+    passwordSize: PropTypes.number.isRequired,
+    setPasswordSize: PropTypes.func.isRequired
+}
+
+export default Input
