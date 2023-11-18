04bfe7893b73ba8c75e1e6e5c743c868f45e7803
athena272 <guilhermera272@gmail.com>
2023-11-18 13:23:46 -0300

    Update App.jsx

--- 010_React/code-10/src/App.jsx
+++ 010_React/code-10/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Input from './components/Input/Input'
+import Checkbox from './components/Checkbox/Checkbox'
 import './App.css'
 
 const App = () => {
   const [password, setPassword] = useState("Sua senha aqui!")
   const [passwordSize, setPasswordSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
   const [copyText, setCopyText] = useState("Copiar")
 
   function generatePassword() {
@@ -26,6 +28,7 @@ const App = () => {
   return (
     <>
       <h1>Gerador de senhas</h1>
+      <Checkbox showInput={showInput} setShowInput={setShowInput} />
       <div>
         <label htmlFor="passwordSize">Tamanho: </label>
         <Input passwordSize={passwordSize > 50 ? setPasswordSize(50) : passwordSize} setPasswordSize={setPasswordSize} />
