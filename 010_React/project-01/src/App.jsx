643598124e74e3a05ed5eab0aa558a821a1c6dcd
athena272 <guilhermera272@gmail.com>
2023-11-18 17:43:52 -0300

    add project-01

---
+++ 010_React/project-01/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+const App = () => {
+  const [games, setGames] = useState(() => {
+    const storedGames = localStorage.getItem("obc-game-lib")
+    if (!storedGames) return []
+    return JSON.parse(storedGames)
+  })
+
+  const [title, setTitle] = useState('')
+  const [cover, setCover] = useState('')
+
+  const addGame = ({ title, cover }) => {
+    const id = Math.floor(Math.random() * 1000000)
+    const game = { id, title, cover }
+    setGames(state => {
+      const newState = [...state, game]
+      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
+      return newState
+    })
+  }
+
+  const removeGame = (id) => setGames(state => {
+    const newState = state.filter(game => game.id !== id)
+    localStorage.setItem("obc-game-lib", JSON.stringify(newState))
+    return newState
+  })
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+    addGame({ title, cover })
+    console.log({ title, cover })
+    setTitle("")
+    setCover("")
+  }
+
+  return (
+    <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Título:</label>
+          <input type="text" id="title" value={title} onChange={(ev) => setTitle(ev.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="cover">Capa:</label>
+          <input type="text" id="cover" value={cover} onChange={(ev) => setCover(ev.target.value)} />
+        </div>
+        <button>Adicionar</button>
+      </form>
+      <div className="games">
+        {games.map((game) => (
+          <div key={game.id} id={`game-${game.id}`}>
+            <img src={game.cover} alt="Capa do jogo" />
+            <div>
+              <h2>{game.title}</h2>
+              <button onClick={() => removeGame(game.id)}>
+                Remover
+              </button>
+            </div>
+          </div>))}
+      </div>
+    </div>
+  )
+}
+
+export default App
