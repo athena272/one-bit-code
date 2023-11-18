ad0a8455192d755d73647932c11eaf8874208134
athena272 <guilhermera272@gmail.com>
2023-11-18 00:05:23 -0300

    add code-10

---
+++ 010_React/code-10/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import Input from './components/Input/Input'
+import './App.css'
+
+const App = () => {
+  const [password, setPassword] = useState("Sua senha aqui!")
+  const [passwordSize, setPasswordSize] = useState(12)
+  const [copyText, setCopyText] = useState("Copiar")
+
+  function generatePassword() {
+    const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+    let newPassword = ""
+    for (let i = 0; i < passwordSize; i++) {
+      const position = Math.floor(Math.random() * characters.length)
+      newPassword += characters[position]
+    }
+    setPassword(newPassword)
+    setCopyText("Copiar")
+  }
+
+  function copyToClipboard() {
+    window.navigator.clipboard.writeText(password)
+    setCopyText("Copiado!")
+  }
+
+  return (
+    <>
+      <h1>Gerador de senhas</h1>
+      <div>
+        <label htmlFor="passwordSize">Tamanho: </label>
+        <Input passwordSize={passwordSize} setPasswordSize={setPasswordSize} />
+      </div>
+      <button onClick={generatePassword}>Gerar senha de {passwordSize} caracteres</button>
+      <button onClick={copyToClipboard}>{copyText}</button>
+      <div>{password}</div>
+    </>
+  )
+}
+
+export default App
