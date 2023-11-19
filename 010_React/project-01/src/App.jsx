865a8016652eb8087935724a027df79c67149486
athena272 <guilhermera272@gmail.com>
2023-11-18 21:36:06 -0300

    Update App.jsx

--- 010_React/project-01/src/App.jsx
+++ 010_React/project-01/src/App.jsx
@@ -1,65 +1,25 @@
-import { useState } from "react"
+import Game from "./components/Game/Game"
+import GameForm from "./components/GameForm/GameForm"
+import useGameCollection from "./hooks/useGameCollection"
 import './App.css'
 
 const App = () => {
-  const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obc-game-lib")
-    if (!storedGames) return []
-    return JSON.parse(storedGames)
-  })
-
-  const [title, setTitle] = useState('')
-  const [cover, setCover] = useState('')
-
-  const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 1000000)
-    const game = { id, title, cover }
-    setGames(state => {
-      const newState = [...state, game]
-      localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-      return newState
-    })
-  }
-
-  const removeGame = (id) => setGames(state => {
-    const newState = state.filter(game => game.id !== id)
-    localStorage.setItem("obc-game-lib", JSON.stringify(newState))
-    return newState
-  })
-
-  const handleSubmit = (ev) => {
-    ev.preventDefault()
-    addGame({ title, cover })
-    console.log({ title, cover })
-    setTitle("")
-    setCover("")
-  }
+  const { games, addGame, removeGame } = useGameCollection()
 
   return (
     <div className="app">
       <h1>Biblioteca de Jogos</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Título:</label>
-          <input type="text" id="title" value={title} onChange={(ev) => setTitle(ev.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="cover">Capa:</label>
-          <input type="text" id="cover" value={cover} onChange={(ev) => setCover(ev.target.value)} />
-        </div>
-        <button>Adicionar</button>
-      </form>
+      <GameForm addGame={addGame} />
       <div className="games">
-        {games.map((game) => (
-          <div key={game.id} id={`game-${game.id}`}>
-            <img src={game.cover} alt="Capa do jogo" />
-            <div>
-              <h2>{game.title}</h2>
-              <button onClick={() => removeGame(game.id)}>
-                Remover
-              </button>
-            </div>
-          </div>))}
+        {games.map(game => (
+          <Game
+            key={game.id}
+            id={game.id}
+            title={game.title}
+            cover={game.cover}
+            onRemove={() => removeGame(game.id)}
+          />
+        ))}
       </div>
     </div>
   )
