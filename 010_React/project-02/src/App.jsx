173b15fe4d50816c3877bc0c4589a95252590ad7
athena272 <guilhermera272@gmail.com>
2023-11-19 10:25:26 -0300

    Update App.jsx

--- 010_React/project-02/src/App.jsx
+++ 010_React/project-02/src/App.jsx
@@ -2,26 +2,58 @@
 import { useState } from "react"
 
 const App = () => {
-  
+  const [comments, setComments] = useState([])
+  const [author, setAuthor] = useState("")
+  const [content, setContent] = useState("")
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    const newComment = {
+      id: Math.floor(Math.random() * 1000000),
+      author: author,
+      content: content,
+      createdAt: new Date()
+    }
+
+    console.log({ newComment })
+    setComments(state => [...state, newComment])
+    setAuthor("")
+    setContent("")
+  }
+
   return (
     <div id="app">
       <h2>Seção de Comentários</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="author">Email</label>
-        <input type="text" id="author" required />
+        <input
+          type="text" id="author" required
+          value={author} onChange={(e) => setAuthor(e.target.value)}
+        />
         <label htmlFor="content">Comentário</label>
-        <textarea id="content" cols="30" rows="6" required></textarea>
+        <textarea
+          id="content" cols="30" rows="6" required
+          value={content} onChange={(e) => setContent(e.target.value)}
+        ></textarea>
         <button>Enviar comentário</button>
       </form>
       <hr />
       <section id="comments">
-        <div>
-          <h3>autor@email.com</h3>
-          <span>Em 01/01/2001</span>
-          <p>Comentário de exemplo</p>
-        </div>
+        {comments.length > 0 ?
+          comments.map(comment => (
+            <div key={comment.id}>
+              <h3>{comment.author}</h3>
+              <span>Em {comment.createdAt.toLocaleString()}</span>
+              <p>{comment.content}</p>
+            </div>
+          ))
+          :
+          <p>Seja o primeiro a comentar :)</p>
+        }
       </section>
     </div>
   )
 }
+
 export default App
