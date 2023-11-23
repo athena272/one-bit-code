2825e6a66703cad74eb0298be1b75696e1c750bb
athena272 <guilhermera272@gmail.com>
2023-11-23 00:42:13 -0300

    Update App.jsx

--- 010_React/code-14/src/App.jsx
+++ 010_React/code-14/src/App.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+async function fetchPokemon() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/")
+  const data = await response.json()
+  return data.results
+}
+
+const App = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    fetchPokemon().then(result => {
+      console.log("REQUISIÇÃo REALIZADA")
+      console.log(result)
+      setPokemons(result)
+    })
+  }, [])
 
   return (
-    <>
+    <div className="app">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Pokémon</h2>
+        <ul className="pokemon">
+          {pokemons.map(pokemon => (
+            <li key={pokemon.name}>
+              <span>{pokemon.name}</span>
+              <button>
+                Ver detalhes
+              </button>
+
+            </li>
+          ))}
+        </ul>
+
+        {JSON.stringify(pokemons)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
