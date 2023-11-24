3c377a0c21d4836ad3f4911f81b5bd08a67df832
athena272 <guilhermera272@gmail.com>
2023-11-24 12:10:47 -0300

    Update App.jsx

--- 010_React/code-15/src/App.jsx
+++ 010_React/code-15/src/App.jsx
@@ -9,86 +9,94 @@ async function fetchPokemon() {
 
 function App() {
   const [pokemons, setPokemons] = useState([])
-  const [url, setUrl] = useState(null)
   const [pokemonShown, setPokemonShown] = useState(null)
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   fetchPokemon().then(result => {
+  //     console.log("Requisição realizada")
+  //     console.log(result)
+  //     setPokemons(result)
+  //   })
+  // }, [])
+
+  if (pokemons.length === 0) {
     fetchPokemon().then(result => {
       console.log("Requisição realizada")
       console.log(result)
       setPokemons(result)
     })
-  }, [])
+  }
 
-  useEffect(() => {
-    if (url) {
-      fetch(url).then(res => res.json()).then(data => {
-        console.log("pokemon encontrado")
-        console.log(data)
-        setPokemonShown(data)
-      })
-    }
-  }, [url])
+  const showDetails = async (url) => {
+    const data = await fetch(url).then(result => result.json())
+    console.log(data)
+    setPokemonShown(data)
+  }
 
   return (
-    <div className="app">
-      <div>
-        <h2>Pokémon</h2>
-        <ul className="pokemon">
-          {pokemons.map(pokemon => (
-            <li key={pokemon.name}>
-              <span>{pokemon.name}</span>
-              <button onClick={() => setUrl(pokemon.url)}>
-                Ver detalhes
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {pokemonShown && (
+    <>
+      <div className="app">
         <div>
-          <h2>{pokemonShown.name}</h2>
-          <img
-            src={pokemonShown.sprites.front_default}
-            alt=""
-          />
-          <div className="stat">
-            <b>Tipo: </b>
-            {pokemonShown.types.map(({ type }) => (
-              <span key={type.name}>{type.name} </span>
+          <h2>Pokémon</h2>
+          <ul className="pokemon">
+            {pokemons.map(pokemon => (
+              <li key={pokemon.name}>
+                <span>{pokemon.name}</span>
+                <button onClick={() => showDetails(pokemon.url)}>
+                  Ver detalhes
+                </button>
+              </li>
             ))}
-          </div>
-          <div className="stat">
-            <b>Altura: </b>{pokemonShown.height / 10} m
-          </div>
-          <div className="stat">
-            <b>Peso: </b>{pokemonShown.weight / 10} Kg
-          </div>
-          <div className="stat">
-            <b>Atributos</b>
-            <ul>
-              {pokemonShown.stats.map(({ base_stat, stat }) => (
-                <li key={stat.name}>
-                  {stat.name}: {base_stat}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="stat">
-            <b>Habilidades</b>
-            <ul>
-              {pokemonShown.abilities.map(({ ability, is_hidden }) => (
-                <li key={ability.name}>
-                  {ability.name}
-                  {is_hidden && " (secreta)"}
-                </li>
-              ))}
-            </ul>
-          </div>
+          </ul>
         </div>
-      )}
-    </div >
+
+        {pokemonShown && (
+          <div>
+            <h2>{pokemonShown.name}</h2>
+            <img
+              src={pokemonShown.sprites.front_default}
+              alt=""
+            />
+            <div className="stat">
+              <b>Tipo: </b>
+              {pokemonShown.types.map(({ type }) => (
+                <span key={type.name}>{type.name} </span>
+              ))}
+            </div>
+            <div className="stat">
+              <b>Altura: </b>{pokemonShown.height / 10} m
+            </div>
+            <div className="stat">
+              <b>Peso: </b>{pokemonShown.weight / 10} Kg
+            </div>
+            <div className="stat">
+              <b>Atributos</b>
+              <ul>
+                {pokemonShown.stats.map(({ base_stat, stat }) => (
+                  <li key={stat.name}>
+                    {stat.name}: {base_stat}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="stat">
+              <b>Habilidades</b>
+              <ul>
+                {pokemonShown.abilities.map(({ ability, is_hidden }) => (
+                  <li key={ability.name}>
+                    {ability.name}
+                    {is_hidden && " (secreta)"}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+      </div >
+      <div>
+        {JSON.stringify(pokemons)}
+      </div>
+    </>
   )
 }
 
