6f1825d2d7ff150c7153573acc05256009576fc8
athena272 <guilhermera272@gmail.com>
2023-11-25 11:08:16 -0300

    Update Products.jsx

--- 010_React/code-17/src/components/Products/Products.jsx
+++ 010_React/code-17/src/components/Products/Products.jsx
@@ -1,15 +1,8 @@
-import { Link } from 'react-router-dom'
 import products from '../../database.json'
 
 const Product = () => (
 
     <section>
-        <header>
-            <nav style={{ display: "flex", gap: "2rem" }}>
-                <Link to="/products">Produtos</Link>
-                <Link to="/cart">Carrinho</Link>
-            </nav>
-        </header>
         <h2>Todos os produtos</h2>
         <p>Confira todas as nossas ofertas.</p>
         <section className="products">
@@ -19,7 +12,7 @@ const Product = () => (
                     <li key={product.id}>
                         <h4>{product.name}</h4>
                         <p>R$ {product.price}</p>
-                        <button>Ver</button>
+                        <button style={{ marginRight: '10px' }}>Ver</button>
                         <button>Compras</button>
                     </li>
                 ))}
