81f5effe4b96527ae031f2016ab6bd4d8930f57d
athena272 <guilhermera272@gmail.com>
2023-11-25 10:21:00 -0300

    Create Admin.jsx

---
+++ 010_React/code-17/src/components/Admin/Admin.jsx
@@ -0,0 +1,8 @@
+const Admin = () => (
+    <main>
+        <h1>Área Administrativa</h1>
+        <p>Esta é a área de administração da loja. Aqui você poderá adicionar produtos e ver os pedidos.</p>
+    </main>
+)
+
+export default Admin
