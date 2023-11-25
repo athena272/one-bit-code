2d65ef17c4d1dcb3042a877334922ebcd8f06096
athena272 <guilhermera272@gmail.com>
2023-11-25 10:20:56 -0300

    Create Cart.jsx

---
+++ 010_React/code-17/src/components/Cart/Cart.jsx
@@ -0,0 +1,17 @@
+const Cart = () => (
+    <section>
+        <h2>Carrinho</h2>
+        <p>Os produtos atualmente em seu carrinho.</p>
+        <ul>
+            <li>2 un. - Memória RAM DDR4 8GB</li>
+            <li>1 un. - Processador Intel Core i3 12th Gen</li>
+            <li>1 un. - SSD Sata 240 GB</li>
+            <li>1 un. - Fonte ATX 500W</li>
+            <li>1 un. - Placa Mãe Intel LGA 1700</li>
+            <li>1 un. - Gabinete</li>
+        </ul>
+        <button>Finalizar compra</button>
+    </section>
+)
+
+export default Cart
