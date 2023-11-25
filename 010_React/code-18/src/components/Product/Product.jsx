139c5e7569fa85201ee6e0314597493555545b1b
athena272 <guilhermera272@gmail.com>
2023-11-25 20:16:07 -0300

    Update Product.jsx

--- 010_React/code-18/src/components/Product/Product.jsx
+++ 010_React/code-18/src/components/Product/Product.jsx
@@ -1,15 +1,27 @@
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
+import products from '../../database.json'
 
-const Product = () => (
-    <section>
-        <Link to="/products">
-            <button>Voltar</button>
-        </Link>
-        <h2>nome do produto</h2>
-        <p>descrição do produto</p>
-        <p>R$ 00,00</p>
-        <button>Comprar</button>
-    </section>
-)
+const Product = () => {
+    const { productId } = useParams()
 
+    const product = products.find(product => product.id === productId)
+
+    if (!product) {
+        return (
+            <h2>Oops... Esse produto não foi encontrado =(</h2>
+        )
+    }
+
+    return (
+        <section>
+            <Link to="/products">
+                <button>Voltar</button>
+            </Link>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <button>Comprar</button>
+        </section>
+    )
+}
 export default Product
