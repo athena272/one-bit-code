18b6f2b89c4d2208d86bb859d22136de0cb62f73
athena272 <guilhermera272@gmail.com>
2023-12-03 22:53:45 -0300

    Update App.vue

--- 011_Vue/code-02/src/App.vue
+++ 011_Vue/code-02/src/App.vue
@@ -1,17 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button type="button" v-on:click="count++">Contador: {{ count }}</button>
 </template>
 
 <style scoped>
@@ -21,9 +19,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
