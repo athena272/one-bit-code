fb7b1de7020d065fc6893025512c2244ae9962dc
athena272 <guilhermera272@gmail.com>
2024-03-16 15:23:48 -0300

    Update index.ts

--- 011_Typescript/code-01/index.ts
+++ 011_Typescript/code-01/index.ts
@@ -2,3 +2,12 @@ const spaceShip = {
     name: 'X-Wing',
     speed: 140,
 }
+
+function accelerate(spaceShip: { name: string; speed: number }, speed: number) {
+    spaceShip.speed += speed
+}
+
+accelerate(spaceShip, 40)
+accelerate(spaceShip, 40)
+
+console.log(spaceShip)
