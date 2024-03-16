c3d1b3a08d5948099ef1e89605103bd03eb1eefb
athena272 <guilhermera272@gmail.com>
2024-03-16 15:27:23 -0300

    Update index.js

--- 011_Typescript/code-01/index.js
+++ 011_Typescript/code-01/index.js
@@ -2,9 +2,9 @@ var spaceShip = {
     name: 'X-Wing',
     speed: 140,
 };
-function accelerate(spaceShip, speed) {
+function accelerateTeste(spaceShip, speed) {
     spaceShip.speed += speed;
 }
-accelerate(spaceShip, 40);
-accelerate(spaceShip, 40);
+accelerateTeste(spaceShip, 40);
+accelerateTeste(spaceShip, 40);
 console.log(spaceShip);
