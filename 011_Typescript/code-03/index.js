e2f5895c12d13cea64093fac73a26a4d56f455f4
athena272 <guilhermera272@gmail.com>
2024-03-16 16:36:30 -0300

    Create index.js

---
+++ 011_Typescript/code-03/index.js
@@ -0,0 +1,25 @@
+function sendSpaceship(name, captain) {
+    var spaceship = {
+        name: name,
+        captain: captain,
+        speed: 20,
+        inMission: true,
+        crew: []
+    };
+    alert("A nave ".concat(spaceship.name, " comandada pelo capit\u00E3o ").concat(spaceship.captain, " foi enviada em uma miss\u00E3o."));
+    return spaceship;
+}
+function accelerate(targetSpeed, spaceship) {
+    if (spaceship.speed > targetSpeed) {
+        alert("Reduzindo a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+    }
+    else if (spaceship.speed < targetSpeed) {
+        alert("Aumentando a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+    }
+    else {
+        alert("Mantendo a velocidade da ".concat(spaceship.name, "..."));
+    }
+}
+var spaceshipName = prompt('Insira o nome da nave a ser enviada:');
+var spaceshipCaptain = prompt('Insira o nome do capitão da nave:');
+sendSpaceship(spaceshipName, spaceshipCaptain);
