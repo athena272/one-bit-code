e52e7ef5a684a695a485b8399ccdac0dbdbe965c
athena272 <guilhermera272@gmail.com>
2024-06-19 00:29:29 -0300

    Create emptyArray.js

---
+++ emptyArray.js
@@ -0,0 +1,17 @@
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+array.splice(0, array.length)
+
+array.length = 0
+
+console.log(array.length = 0)
+
+var x
+var y = 1
+x = y = typeof x
+console.log("🚀 ~ x:", x)
+
+y = 1
+x = y
+console.log("🚀 ~ typeof x:", typeof x)
+
