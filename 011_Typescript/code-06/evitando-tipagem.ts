f11531b4ed28b821d6610275a5ae1a899509682a
athena272 <guilhermera272@gmail.com>
2024-06-22 14:18:10 -0300

    Create evitando-tipagem.ts

---
+++ 011_Typescript/code-06/evitando-tipagem.ts
@@ -0,0 +1,7 @@
+function sendSpaceship(spaceship: { pilot: string, copilot?: string }) {
+    //...
+}
+
+sendSpaceship({ pilot: "Han Solo", copilot: "Chewbacca" })
+
+sendSpaceship({ pilot: "Luke Skywalker" })
