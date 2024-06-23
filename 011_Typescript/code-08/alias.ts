b15e8d402295283f4e969122c0f1545189ad68b8
athena272 <guilhermera272@gmail.com>
2024-06-23 15:14:30 -0300

    Update alias.ts

--- 011_Typescript/code-08/alias.ts
+++ 011_Typescript/code-08/alias.ts
@@ -17,3 +17,22 @@ function checkPlanet(planet: Planet) {
         console.log("Estamos na Terra")
     }
 }
+
+// No TypeScript também podemos definir um Alias para uma função
+// Podemos fazer isso através da sintaxe de arrow functions
+// mencionando os parâmetros e o tipo de retorno
+type GreetingCallback = (name: string) => void
+
+// A partir disso podemos anotar esse tipo
+function greet(callback: GreetingCallback) {
+    const name = prompt("Qual é o seu nome?")
+    if (name === null) return
+
+    callback(name)
+}
+
+// E ao definir nosso callback temos acesso às informações
+// sobre o seu tipo e retorno para nos auxiliar
+greet((name) => {
+    alert(`Olá, ${name}! Essa é uma saudação executada como callback!`)
+})
