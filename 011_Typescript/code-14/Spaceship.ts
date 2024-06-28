0fa0d604cdeebf0808cfb5273cd8e2002b76bba8
athena272 <guilhermera272@gmail.com>
2024-06-28 00:19:45 -0300

    Update Spaceship.ts

--- 011_Typescript/code-14/Spaceship.ts
+++ 011_Typescript/code-14/Spaceship.ts
@@ -1,16 +1,28 @@
 class Spaceship {
-    name: string
-    captain: string
-    speed: number
+    // Modificamos a propriedade para não haver conflito com os
+    // accessors já que _name nunca vai ser usado fora da classe
+    private _name: string
+    protected captain: string
+    protected speed: number
 
     constructor({ name, captain }: { name: string, captain: string }) {
-        this.name = name
+        this._name = name
         this.captain = captain
         this.speed = 0
     }
 
-    accelerate({ rate, time }: { rate: number, time: number }) {
+    public accelerate({ rate, time }: { rate: number, time: number }) {
         this.speed = rate * time
+    }
+
+    // accessor get
+    get name() {
+        return this._name
+    }
+
+    // accessor set
+    set name(name: string) {
+        this._name = name
     }
 }
 
@@ -34,3 +46,7 @@ let ship = new Spaceship({
     name: "USS Enterprise",
     captain: "James T. Kirk"
 })
+
+ship.accelerate({ rate: 2, time: 16 })
+console.log("🚀 ~ ship.accelerate:", ship)
+
